75337f6ed889feeb93bf4ebe97a5d0adb083f0c6
Mecha komasan <moussaender09@gmail.com>
2025-10-07 16:18:39 -0400

    Create HelloWorld.java
    Printed Hello Tyler and Moussa

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class HelloWorld extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Tyler and Moussa");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
