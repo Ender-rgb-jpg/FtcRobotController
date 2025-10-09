d0914f20fc4b39f40a3451a36f73e980c9918c5d
Mecha komasan <moussaender09@gmail.com>
2025-10-09 14:51:55 -0400

    Create VariablePractice.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VariablePractice extends OpMode {
+    @Override
+    public void init() {
+        int teamNumber = 16903;
+        double motorSpeed = 0.75;
+        boolean clawClosed = true;
+        String mechaRavens = "Tyler and Moussa";
+        int motorAngle = 60;
+
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Motor Speed", motorSpeed);
+        telemetry.addData("Claw State", clawClosed);
+        telemetry.addData("Name", mechaRavens);
+        telemetry.addData("Motor Angle", motorAngle);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
